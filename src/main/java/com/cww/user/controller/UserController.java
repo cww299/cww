78762fb70ba1cc9daeec7a440956d259829325f7
cww <chenxxvv@foxmail.com>
2019-09-10 17:25:49 +0800

    接口修改

--- src/main/java/com/cww/user/controller/UserController.java
+++ src/main/java/com/cww/user/controller/UserController.java
@@ -6,18 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cww.user.dao.UserDao;
 import com.cww.user.pojo.User;
 
 
-@Controller
+@RestController
 public class UserController {
 	@Autowired 
 	UserDao userDao;
 	
 	@RequestMapping("/listUser")
 	public String listUser(Model m) throws Exception{
+		/*
+		 * 将接口设置为异步查询、用于测试
+		 */
 		List<User> us = userDao.findAll();
 		m.addAttribute("userList",us);
 		return "listUser";
